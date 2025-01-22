fcfe5dd52a3603781d1819a44854f254a6e921e6
Vijay Saddi <vijaysaddi@vijays-mbp.home>
2025-01-22 14:42:46 -0500

    feat: add before and after node

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -3,6 +3,9 @@ class Node:
         self.data = data
         self.next = None
         self.prev = None
+    
+    def __str__(self):
+        return f"prev: {self.prev}, next: {self.next}, data: {self.data}"
 
 class DoublyLinkedList:
     def __init__(self):
@@ -33,6 +36,39 @@ class DoublyLinkedList:
             new_node.next = self.head
             self.head = new_node
             new_node.prev = None
+    
+    def add_after_node(self, key, data) -> None:
+        cur = self.head
+        while cur:
+            if cur.next is None and cur.data == key:
+                self.append(data)
+                return
+            elif cur.data == key:
+                new_node = Node(data)
+                next = cur.next
+                cur.next = new_node
+                new_node.next = next
+                new_node.prev = cur
+                next.prev = new_node
+                # print(new_node.__str__())
+
+            cur = cur.next
+
+    def add_before_node(self, key, data) -> None:
+        cur = self.head
+        while cur:
+            if cur.prev is None and cur.data == key:
+                self.prepend(data)
+                return
+            elif cur.data == key:
+                new_node = Node(data)
+                prev = cur.prev
+                prev.next = new_node
+                cur.prev = new_node
+                new_node.next = cur
+                new_node.prev = prev
+            
+            cur = cur.next
 
     def print_list(self):
         cur = self.head
@@ -48,4 +84,8 @@ if __name__ == "__main__":
     dll.append(3)
     dll.append(4)
     dll.prepend(-1)
+    dll.add_after_node(1, 11)
+    dll.add_after_node(3, 99)
+    dll.add_before_node(3, 77)
+
     dll.print_list()
